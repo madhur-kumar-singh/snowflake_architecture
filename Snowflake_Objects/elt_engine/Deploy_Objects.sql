ace8acfd09b9a1eb9ae8e91124de32d8e114032f
Madhur Singh <madhursingh@Madhurs-MacBook-Air.local>
2024-12-27 12:30:54 +0530

    updated deploy object

--- Snowflake_Objects/elt_engine/Deploy_Objects.sql
+++ Snowflake_Objects/elt_engine/Deploy_Objects.sql
@@ -1,5 +1,5 @@
 BEGIN
-    EXECUTE IMMEDIATE FROM @elt_engine.public.github_repo/branches/main/Snowflake_Objects/elt_engine/sequence_script.sql;
-    EXECUTE IMMEDIATE FROM @elt_engine.public.github_repo/branches/main/Snowflake_Objects/elt_engine/table_script.sql;
-    EXECUTE IMMEDIATE FROM @elt_engine.public.github_repo/branches/main/Snowflake_Objects/elt_engine/procedure_script.sql;
+    EXECUTE IMMEDIATE FROM @elt_engine.public.github_repo/branches/main/Snowflake_Objects/elt_engine/Sequence_Script.sql;
+    EXECUTE IMMEDIATE FROM @elt_engine.public.github_repo/branches/main/Snowflake_Objects/elt_engine/Table_Script.sql;
+    EXECUTE IMMEDIATE FROM @elt_engine.public.github_repo/branches/main/Snowflake_Objects/elt_engine/Procedure_Script.sql;
 END;
